84be16753e7220a22c765175c359f18b1ea6381c
afhistos-git <32820096+afhistos@users.noreply.github.com>
2021-10-10 21:56:44 +0200

    Added Header in index.php

---
+++ index.php
@@ -0,0 +1,56 @@
+<?php
+$main_url = "#";
+session_start();
+if(!isset($_SESSION['id']) || empty($_SESSION['id'])){
+    header('Status: 401	Unauthorized', false, 401);
+    header('Location: login/index.php');
+    exit();
+}
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienvenue sur Vakardia</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
+</head>
+<body>
+<nav class="navbar" role="navigation">
+    <div class="navbar-brand">
+        <a class="navbar-item" href="<?php echo $main_url ?>">
+            <img src="resources/logo.webp" width="64" height="64">
+        </a>
+        <a role="button" class="navbar-burger" data-target="navbarMain">
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+        </a>
+    </div>
+    <div id="navbarMain" class="navbar-menu">
+        <div class="navbar-start">
+            <a class="navbar-item" href="index.php">Accueil</a>
+            <a class="navbar-item" href="/console/index.php">Console</a>
+            <div class="navbar-item has-dropdown is-hoverable">
+                <a class="navbar-link" href="/stats/index.php">Statistiques</a>
+                <div class="navbar-dropdown">
+                    <a class="navbar-item" href="/stats/server.php">Serveur</a>
+                    <a class="navbar-item" href="/stats/players.php">Joueurs</a>
+                    <a class="navbar-item" href="/stats/site.php">Site web</a>
+                </div>
+            </div>
+            <a class="navbar-item" href="/players/staff.php">Staff</a>
+            <a class="navbar-item" href="/players/index.php">Joueurs</a>
+        </div>
+        <div class="navbar-end">
+            <div class="navbar-item">
+                <div class="buttons">
+                    <a class="button is-info">Se déconnecter</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</nav>
+</body>
+</html>
