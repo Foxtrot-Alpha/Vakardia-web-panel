23a0726124fc80e5e4e48c8a43899528771a98d3
afhistos-git <32820096+afhistos@users.noreply.github.com>
2021-10-20 15:54:46 +0200

    Finished addAccount.php

--- login/addAccount.php
+++ login/addAccount.php
@@ -1,4 +1,5 @@
 <?php
+include_once($_SERVER['DOCUMENT_ROOT'].'/db.php');
 session_start();
 if(isset($_SESSION['id']) && !empty($_SESSION['id'])){
     header('Location: /index.php');
@@ -9,16 +10,37 @@ if(!isset($_POST['mail']) || empty($_POST['mail']) || !isset($_POST['password'])
     header('Location: register.php?error=emptyArgs');
     exit();
 }
+$mail = $_POST['mail'];
+$pwd = $_POST['password'];
+
 $mailRegex = '/^(?!(?:(?:\x22?\x5C[\x00-\x7E]\x22?)|(?:\x22?[^\x5C\x22]\x22?)){255,})(?!(?:(?:\x22?\x5C[\x00-\x7E]\x22?)|(?:\x22?[^\x5C\x22]\x22?)){65,}@)(?:(?:[\x21\x23-\x27\x2A\x2B\x2D\x2F-\x39\x3D\x3F\x5E-\x7E]+)|(?:\x22(?:[\x01-\x08\x0B\x0C\x0E-\x1F\x21\x23-\x5B\x5D-\x7F]|(?:\x5C[\x00-\x7F]))*\x22))(?:\.(?:(?:[\x21\x23-\x27\x2A\x2B\x2D\x2F-\x39\x3D\x3F\x5E-\x7E]+)|(?:\x22(?:[\x01-\x08\x0B\x0C\x0E-\x1F\x21\x23-\x5B\x5D-\x7F]|(?:\x5C[\x00-\x7F]))*\x22)))*@(?:(?:(?!.*[^.]{64,})(?:(?:(?:xn--)?[a-z0-9]+(?:-[a-z0-9]+)*\.){1,126}){1,}(?:(?:[a-z][a-z0-9]*)|(?:(?:xn--)[a-z0-9]+))(?:-[a-z0-9]+)*)|(?:\[(?:(?:IPv6:(?:(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){7})|(?:(?!(?:.*[a-f0-9][:\]]){7,})(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,5})?::(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,5})?)))|(?:(?:IPv6:(?:(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){5}:)|(?:(?!(?:.*[a-f0-9]:){5,})(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,3})?::(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,3}:)?)))?(?:(?:25[0-5])|(?:2[0-4][0-9])|(?:1[0-9]{2})|(?:[1-9]?[0-9]))(?:\.(?:(?:25[0-5])|(?:2[0-4][0-9])|(?:1[0-9]{2})|(?:[1-9]?[0-9]))){3}))\]))$/iD';
 $pwdRegex = '^(?=.*[a-z])(?=.*[0-9])(?=.*[!@#\$%\^&\*\"\'\(\)§\{\}°µ£´`,?;\.:\/=+~])(?=.{8,})^';
 $error = "bad";
 
-if(!preg_match($mailRegex, $_POST['mail'])){$error = $error . "Mail";}
-if(!preg_match($pwdRegex, $_POST['password'])){$error= $error . "Pwd";}
+if(!preg_match($mailRegex, $mail)){$error = $error . "Mail";}
+if(!preg_match($pwdRegex, $pwd)){$error= $error . "Pwd";}
 if(strlen($error)> 3){
     header("Location: register.php?error={$error}");
     exit();
 }else{
-    echo "Username: {$_POST['username']}\nMail: {$_POST['mail']}\nPassword: {$_POST['password']}";
+    $username= empty($_POST['username']) ? explode("@", $mail)[0] : $_POST['username'];
+    $finalPwd = $hashPwd($pwd, $mail);
+    echo "Passed form verif\n";
+    //Vérifier si l'adresse mail est déjà présente
+    if(!mysqli_query($db, "SELECT id FROM users WHERE use_email='$mail'")){
+        echo "Unused mail\n";
+        if($result = mysqli_query($db, "INSERT INTO users (use_email, use_username, use_password) VALUES ('$mail', '$username', '$pwd')")){
+            echo "INSERT successful";
+            $_SESSION['mail'] = $mail;
+            $_SESSION['username'] = $username;
+            $_SESSION['avatar'] = '/resources/users/default.png'; 
+            header('Location: success.php');
+        }
+        echo "Value of errors: ". $db->errno . "Content: ". $db->error;
+
+    }else{
+        header('Location: register.php?error=usedMail');
+        exit();
+    }
 }
 ?>
---
+++ login/success.php
@@ -0,0 +1,4 @@
+<?php
+session_start();
+echo "Success!\nUsername:".$_SESSION['username'];
+?>
