df26b436dfe449db9c150825f3266773e10e1ea8
afhistos-git <32820096+afhistos@users.noreply.github.com>
2021-10-21 22:09:20 +0200

    Fixed id storage

--- login/connect.php
+++ login/connect.php
@@ -17,12 +17,12 @@ if(isset($_SESSION['id']) && !empty($_SESSION['id'])){
     if(isset($_POST['mail']) && isset($_POST['password']) && !empty($_POST['mail'] && !empty($_POST['password']))){
         $mail = $_POST['mail'];
         $password = $hashPwd($_POST['password'], $mail);
-        $results = mysqli_query($db, "SELECT use_username, use_avatar FROM users WHERE use_email='$mail' AND use_password='$password'");
+        $results = mysqli_query($db, "SELECT use_username, use_avatar, use_id FROM users WHERE use_email='$mail' AND use_password='$password'");
         if($results && $results->num_rows !== 0){
             $row = mysqli_fetch_assoc($results);
             $_SESSION['username'] = $row['use_username'];
             $_SESSION['avatar'] = $row['use_avatar'];
-            $_SESSION['id'] = mysqli_insert_id($db);
+            $_SESSION['id'] = $row['use_id'];
             mysqli_free_result($results);
             header('Location: success.php');
             exit();
