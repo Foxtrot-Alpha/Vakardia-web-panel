b0b98043cd7b64d909b8ec2fe973212bec56819b
afhistos-git <32820096+afhistos@users.noreply.github.com>
2021-10-27 15:50:58 +0200

    Added global.css

--- index.php
+++ index.php
@@ -15,6 +15,7 @@ if(!isset($_SESSION['id']) || empty($_SESSION['id'])){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bienvenue sur Vakardia</title>
     <link rel="stylesheet" href="https://unpkg.com/bulmaswatch/darkly/bulmaswatch.min.css">
+    <link rel="stylesheet" href="global.css">
     <style>
         #outer{
             width: 100%;
